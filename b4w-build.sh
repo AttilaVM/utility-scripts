4d049b9e8d0cf83510d147e662c0efbdb1b538c8
Attila Molnar <ate.molnar2@gmail.com>
2018-04-25 12:08:46 +0200

    Add: blend4web project builder script

---
+++ b4w-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ ! -r ".b4w_project" ];then
+		echo "No b4w_project file in working directory"
+		exit
+fi
+
+python3 project.py build || exit
+cd build || exit
+
+buildIgnoreContent=$(cat ../.buildignore \
+												 | sed 's|#.*$||g' \
+												 | sed '/^\s*$/d')
+
+for globExpr in ${buildIgnoreContent}; do
+    rm -vrf "$globExpr"
+done
