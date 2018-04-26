88a438e18c119fe10753727b03e53a08651460a1
Attila Molnar <ate.molnar2@gmail.com>
2018-04-26 16:54:54 +0200

    Fix: img2web.sh appends image widths to file names

--- img2web.sh
+++ img2web.sh
@@ -15,11 +15,14 @@ if [ "$2" == "" ]; then
 		exit
 fi
 
-widths=$(echo "320 375 425 768 1024 1440 1920 2560" | tr " " "\n")
+widths=$(echo "320 375 425 768 1024 1440 1920" | tr " " "\n")
 
 cd "$srcDir"
 
+newScaleStr=""
+counter="0"
 for f in *.png; do
+		baseName=$(echo "$f" | sed 's/\.png$//')
 		originalImgWidth=$(identify "$f" \
 													 | tr " " "\n" \
 													 | sed -n '/^[0-9]*x[0-9]*$/p' \
@@ -27,7 +30,13 @@ for f in *.png; do
 		
 		for width in ${widths}; do
 				newScale=$(echo "$originalImgWidth*$width/1920" | bc)
-				convert "$f" -resize "$newScale$x$newScale" "$dstDir/$f-$newScale.png"
-				convert "$f" -resize "$newScale$x$newScale" "$dstDir/$f-$newScale.webp"
+				convert "$f" -resize "$newScale$x$newScale" "$dstDir/$baseName-$newScale.png"
+				convert "$f" -resize "$newScale$x$newScale" "$dstDir/$baseName-$newScale.webp"
+				if [ "$counter" -lt "1" ]; then
+						newScaleStr="$newScaleStr $newScale"
+				fi
 		done
+		counter=$((counter+1))
 done
+
+echo "$newScaleStr"
